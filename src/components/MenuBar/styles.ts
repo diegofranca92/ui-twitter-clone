90c85deb81aadf5ccb8fa1e587e0706192de2fe1
Diego França <diego.franca92@gmail.com>
2021-02-05 13:09:36 -0300

    Inicio da MenuBar

---
+++ src/components/MenuBar/styles.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import { 
+    Home,
+    Notifications,
+    Email,
+    FavoriteBorder,
+    Person,
+    ExitToApp,
+    Rocketseat
+ } from '../../styles/Icons';
+
+export const Container = styled.div`
+  
+`;
+
+export const Topside = styled.div`
+`;
+
+export const Logo = styled.div`
+`;
+
+export const MenuButton = styled.div`
+`;
+
+
+export const HomeIcon = styled(Home)`
+`;
+
+export const BellIcon = styled(Notifications)`
+`;
+
+export const EmailIcon = styled(Email)`
+`;
+
+export const FavoriteIcon = styled(FavoriteBorder)`
+`;
+
+export const ProfileIcon = styled(Person)`
+`;
