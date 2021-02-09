6a9f3055691ffb84c88a4ef582e1971dfb8bcef4
Diego França <diego.franca92@gmail.com>
2021-02-09 14:51:31 -0300

    build

---
+++ build/precache-manifest.7ccc379644b9d35e3239c2846a6e033c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2343a6929a1f335e9ae7ff8f658fb458",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d81363ee234a0f301fc0",
+    "url": "/static/js/2.ed52f410.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ed52f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3af7f6808b6df2bf20a1",
+    "url": "/static/js/main.44fc0224.chunk.js"
+  },
+  {
+    "revision": "cb137ed6b9692a80a2ab",
+    "url": "/static/js/runtime-main.4e3ab4ca.js"
+  }
+]);
